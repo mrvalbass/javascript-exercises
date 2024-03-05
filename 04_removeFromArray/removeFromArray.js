8c09fed6438e896c77829880a5e87dc0973220eb
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-05 17:41:33 +0100

    Add solution to Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+  for (element of elementsToRemove) {
+    while (array.includes(element)) {
+      array.splice(array.indexOf(element),1)
+    } 
+  }
+  return array                 
 };
 
 // Do not edit below this line

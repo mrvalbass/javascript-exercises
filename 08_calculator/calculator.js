3245733ece0c19013b063c7cc9daeb0014a99957
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-25 14:23:39 +0100

    Add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, val) => acc + val, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((acc, val) => acc * val)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n===0) return 1
+  const array = [...Array(n+1).keys()].slice(1)
+  return multiply(array)
 };
 
 // Do not edit below this line

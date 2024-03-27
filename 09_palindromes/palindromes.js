69ff5afdcf6cbe584ea57999fdacc20343e2c831
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-27 11:27:28 +0100

    Add solution to Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const pattern = /[a-z0-9]/g
+  str = str.toLowerCase().match(pattern).join('')
+  return str === str.split('').reverse().join('')
 };
 
 // Do not edit below this line

a8ab0742e4eec51072ffa6afd99e01e6e20b6fd8
Valentin Guillot <97942568+mrvalbass@users.noreply.github.com>
2024-03-27 21:56:13 +0100

    Add solution to Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const ageArray = people.map(person => {
+    if (person.yearOfDeath) return person.yearOfDeath - person.yearOfBirth
+    return new Date().getFullYear() - person.yearOfBirth
+  })
+  return people[ageArray.findIndex(age => age === Math.max(...ageArray))]
 };
 
 // Do not edit below this line
